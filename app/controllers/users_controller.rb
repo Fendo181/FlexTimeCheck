01f2b6ebff863f3c7b6307e716cc6eb477674906
Fendo181 <fendo181git@gmail.com>
2018-03-01 07:09:16 +0900

    入力フォームの骨組みを作成した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,38 @@
 class UsersController < ApplicationController
   def home
+    @user = User.new
+  end
+
+  def check
+    # 入力値のチェック
+    @user = User.new(user_params)
+    if @user.valid?
+      # OK。確認画面を表示
+      id = @user.id
+      password = @user.password
+
+      
+
+      render 'help'
+    else
+      # NG。入力画面を再表示
+      render 'home'
+    end
   end
 
   def help
   end
+
+  def hm(mins, display_sign: false)
+    sign = mins <=> 0
+    mins = mins.abs
+
+    hours, mins = mins.divmod(60)
+    (display_sign ? (sign >= 0 ? '+' : '-') : '') + (hours > 0 ? "#{hours}h" : '') + "#{mins}m"
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:id, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/home'
-  get 'users/help'
+  get 'home' => 'users#home'
+  post 'check' => 'users#check'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
